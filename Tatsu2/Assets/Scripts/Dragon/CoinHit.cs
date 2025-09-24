762a7a82ace94bc21f3d05af8ba29ab22b4551b0
Momoka Yamada <s2310286.yamada.cgp@gmail.com>
2025-09-25 00:40:46 +0900

    dragon完成

--- Tatsu2/Assets/Scripts/Dragon/CoinHit.cs
+++ Tatsu2/Assets/Scripts/Dragon/CoinHit.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class ReturnButton : MonoBehaviour
+public class CoinHit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -17,12 +16,8 @@ public class ReturnButton : MonoBehaviour
         
     }
 
-    public void OnClick()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (Time.timeScale != 1)
-        {
-            Time.timeScale = 1;
-        }
-        SceneManager.LoadScene("StartScene");
+        gameObject.SetActive(false);
     }
 }
---
+++ Tatsu2/Assets/Scripts/Dragon/HitController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HitController : MonoBehaviour
+{
+    [SerializeField] private GameObject finishUI;
+    [SerializeField] private GameObject failUI; // 追加する
+    [SerializeField] private GameObject cloudEffect;
+    [SerializeField] private TextMeshProUGUI coinText;
+    private int coinCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishUI.SetActive(false);
+        failUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Goal")
+        {
+            finishUI.SetActive(true);
+        }
+        else if (other.gameObject.tag == "Cloud")
+        {
+            cloudEffect.GetComponent<CloudEffectFade>().Play();
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.tag == "Coin")
+        {
+            coinCount++;
+            coinText.text = "×" + coinCount.ToString();
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.tag == "Wind")
+        {
+            failUI.SetActive(true);
+            Time.timeScale = 0; 
+        }
+    }
+}
